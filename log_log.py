69a6eceec9fd460c22b6c10858b045a1522e3d93
Anders Green <anders.green@proceranetworks.com>
2016-05-07 15:29:47 +0200

    small cleanup of log_log.py

--- log_log.py
+++ log_log.py
@@ -2,13 +2,18 @@ import config
 import datetime
 import hashlib
 import json
+import os
 import sectoralarm
 
+
 SECTORSTATUS = sectoralarm.SectorStatus(config)
+
 log = SECTORSTATUS.event_log()
 loghash = hashlib.sha256(json.dumps(log)).hexdigest()
 
 now = datetime.datetime.now()
 
-with open('./data/log_'+now.strftime('%Y%m%d_%H%M%S')+'_'+loghash+'.log', 'w') as logfile:
+LOGFILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'log_'+now.strftime('%Y%m%d_%H%M%S')+'_'+loghash+'.log')
+
+with open(LOGFILE, 'w') as logfile:
     json.dump(log, logfile, indent=4, separators=(',', ': '))
